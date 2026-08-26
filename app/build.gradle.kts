c86bfdea890cf624527ba6ee35c74609593725ea
Hank Liu <hank.l@qpay.com.tw>
2026-08-26 15:16:54 +0800

    Upgrade Version 0.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.qpay.xcash"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 5
+        versionName = "0.0.5"
     }
 
     buildTypes {
